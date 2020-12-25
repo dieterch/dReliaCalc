010c6da8b780605a29031a6c325181bcc6dfa12d
dieterchvatal <dieter.chvatal@gmail.com>
2020-12-25 21:38:31 +0100

    codebase aligned

--- pure.py
+++ pure.py
@@ -56,7 +56,7 @@ def main():
         # demonstrated_Reliabillity_Plot(beta=1.21, T=20000, s=1000, cl=[0.1, 0.5, 0.9])
         failures = pd.DataFrame([])
         pl.demonstrated_Reliabillity_Plot(
-            vl, beta=1.4, T=30000, s=1000, ft=failures, cl=[10, 50, 90], factor=1.0)
+            vl, beta=1.4, T=30000, s=1000, ft=failures, cl=[10, 50, 90], factor=1.2)
 
         logging.info('CUI dmyplantlication Run successfully completed.')
 
